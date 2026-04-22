e8efacdfadede2b3ae9731f87e4d2a438f8ffd60
fku <fabian.kukuck@ipt.ch>
2026-04-22 21:41:32 +0200

    fix(terraform): drop local init artifacts

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_cidr]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aca" {
+  name                 = var.aca_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.aca_subnet_cidr]
+}
+
+resource "azurerm_subnet" "aci" {
+  name                 = var.aci_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.aci_subnet_cidr]
+
+  delegation {
+    name = "aci-delegation"
+
+    service_delegation {
+      name = "Microsoft.ContainerInstance/containerGroups"
+    }
+  }
+}
