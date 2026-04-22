4fccb22ef0e6d1563eb128bd818b72c222cfe9b6
fku <fabian.kukuck@ipt.ch>
2026-04-22 21:41:32 +0200

    fix(terraform): drop local init artifacts

---
+++ terraform/modules/container_apps_env/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = azurerm_container_app_environment.this.id
+}
+
+output "name" {
+  value = azurerm_container_app_environment.this.name
+}
+
+output "default_domain" {
+  value = azurerm_container_app_environment.this.default_domain
+}
+
+output "storage_attachment_name" {
+  value = azurerm_container_app_environment_storage.server.name
+}
